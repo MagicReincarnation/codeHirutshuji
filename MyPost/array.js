f0a443aa81f384d057766c7c3caf5810c9b7ac61
MAGIC Reincarnated <139699919+MagicReincarnation@users.noreply.github.com>
2024-08-19 22:32:47 +0700

    Update array.js

--- MyPost/array.js
+++ MyPost/array.js
@@ -1,6 +1,4 @@
 // Daftar link ke file XML
 const postLinks = [
- "https://github.com/MagicReincarnation/codeHirutshuji/raw/main/MyPost/post/Form%20Login%20Free/post_content.xml",
-    "https://raw.githubusercontent.com/username/repo/branch/path/to/post2.xml",
-    // Tambahkan link XML lainnya
+"https://raw.githubusercontent.com/MagicReincarnation/codeHirutshuji/main/MyPost/post/Form%20Login%20Free/post_content.xml",
 ];
