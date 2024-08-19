8a7faa7edbdda7a5674804b3fd391ff830bce9eb
MAGIC Reincarnated <139699919+MagicReincarnation@users.noreply.github.com>
2024-08-19 22:26:07 +0700

    Update

---
+++ MyPost/array.js
@@ -0,0 +1,6 @@
+// Daftar link ke file XML
+const postLinks = [
+ "https://raw.githubusercontent.com/username/repo/branch/path/to/post1.xml",
+    "https://raw.githubusercontent.com/username/repo/branch/path/to/post2.xml",
+    // Tambahkan link XML lainnya
+];
